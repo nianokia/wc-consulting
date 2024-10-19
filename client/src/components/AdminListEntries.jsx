c7bc40b0134e4f51e7456000b789099d16982df2
Nia Wright <nw.niawright@gmail.com>
2024-10-18 20:40:52 -0400

    Add clarifying comments regarding component's purpose

--- client/src/components/AdminListEntries.jsx
+++ client/src/components/AdminListEntries.jsx
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const AdminListEntries = () => {
   const [client_entries, setClient_Entries] = useState([]);
 
-  const loadEntries = () => {
+  // load all th
+  const loadClientEntries = () => {
     fetch('http://localhost:4545/list')
       .then((response) => response.json())
+      .then((client_entries) => {
+        setClient_Entries(client_entries);
+      })
   }
+
+  useEffect(() => {
+    loadClientEntries();
+  }, []);
 
   return (
     <>
       <h1>Admin List Entries</h1>
+      <ul>
+        {client_entries.map((client_entry) => {
+          return (
+            <li key={client_entry.client_entries_id}>
+              {client_entry.first_name} {client_entry.last_name}
+            </li>
+          )
+        })}
+      </ul>
+      <hr />
+      <Link to='/'>Home</Link>
+      <br />
+      <Link to='/login'>Log Out</Link>
     </>
   )
 }
