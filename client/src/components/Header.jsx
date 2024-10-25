beb85365ebaee6ec0a0f432adc3cd6201362c818
Nia Wright <nw.niawright@gmail.com>
2024-10-25 13:17:58 -0400

    Make homeButton in Header navigate to back to Homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,22 +7,22 @@ import { Link } from 'react-router-dom';
 const Header = () => {
  return (
    <div className="Header" style={{display: "flex", alignItems: "flex-end", justifyContent: "space-between"}}>
-     <div className="homeButton" style={{display: "flex", flexDirection: "column"}}>
-       <div>
-         <img src="../public/logo.png" alt="Wright Choice Consulting logo" style={{width: '40px'}} />
-         <h4 style={{marginTop: "5px", marginBottom: 0}}>Wright Choice Consulting</h4>
-       </div>
-       <div>
-         <h6 style={{margin: "0px"}}>Family Therapy</h6>
-       </div>
-     </div>
+    <Link to='/' className="homeButton link-style" style={{display: "flex", flexDirection: "column", textDecoration: "none"}}>
+      <div>
+        <img src="../public/logo.png" alt="Wright Choice Consulting logo" style={{width: '40px'}} />
+        <h4 style={{marginTop: "5px", marginBottom: 0}}>Wright Choice Consulting</h4>
+      </div>
+      <div>
+        <h6 style={{margin: "0px"}}>Family Therapy</h6>
+      </div>
+    </Link>
      <div className="Navbar" style={{display: "flex", gap: "10px"}}>
-       <Link to='/about'>About</Link>/
-       <Link to='/services'>Services</Link>/
-       <Link to='/rates-and-insurance'>Rates/ Insurance</Link>/
-       <Link to='/faqs'>FAQs</Link>/
-       <Link to='/first-session'>First Session</Link>/
-       <Link to='/contact'>Contact</Link>
+       <Link to='/about' className="link-style">About</Link>/
+       <Link to='/services' className="link-style">Services</Link>/
+       <Link to='/rates-and-insurance' className="link-style">Rates/ Insurance</Link>/
+       <Link to='/faqs' className="link-style">FAQs</Link>/
+       <Link to='/first-session' className="link-style">First Session</Link>/
+       <Link to='/contact' className="link-style">Contact</Link>
      </div>
    </div>
  )
