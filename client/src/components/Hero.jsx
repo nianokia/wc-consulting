0359d13d2c8f8cd53b3274ec3b34f09635a7b115
Nia Wright <nw.niawright@gmail.com>
2024-10-25 14:23:07 -0400

    Conditionally Format Hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,18 +3,21 @@ import React from "react";
 // PURPOSE - display a hero on certain pages with an image, title, and optional button
 // its a reusable component that can be called in many components while being editted in one component (normalizes code)
 
-const Hero = ({ title, image, button }) => {
+const Hero = ({ title, image, button, textalign, tagline }) => {
   return (
-    <>
-      <div style={{backgroundImage: {image}}}>
-        <h1>{title}</h1>
-        {button ? (
-          <div>
-            <button>{button}</button>
-          </div>
-        ) : null}
-      </div>
-    </>
+    
+    <div className="Hero" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)), url(${image})`}}>
+      {textalign === 'center' ? (
+        <>
+          <h1 style={{textAlign: "center", margin: "10px"}}>{title}</h1>
+          <h3 style={{textAlign: "center", margin: "10px 0 20px 0"}}>{tagline}</h3>
+          <button style={{display: "block", margin: "auto"}}>{button}</button>
+        </>
+      ) : (
+        <h1 style={{paddingLeft: "50px"}}>{title}</h1>
+        )
+      }
+    </div>
   )
 }
 
