00f5382d6283e0b9b00965a2f259d3bd66ba3f59
Nia Wright <nw.niawright@gmail.com>
2024-11-04 18:28:23 -0500

    Reinstated previous commit with created and developed test files on the client side

--- client/src/__tests__/ContactHome.test.jsx
+++ client/src/__tests__/ContactHome.test.jsx
@@ -1,7 +1,24 @@
 /**
  * ------ CONTACT HOME TEST ------
- * Render Header
- * Render ContactQuestion component
- * Render div containing business information
- * Render Footer 
+ * Render component (  )
+ * Render div containing business information (  )
  */
+import React from "react";
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { describe, test, expect } from 'vitest';
+import { MemoryRouter } from "react-router-dom";
+import ContactHome from '../pages/ContactHome.jsx';
+
+describe('ContactHome page component', () => {
+    test('display Contact page contents', async () => {
+        const { container } = render(<MemoryRouter><ContactHome /></MemoryRouter>);
+
+        // --- select elements ---
+        const contactHeading = await screen.findByRole('heading', { name: /Contact Home/i });
+        const hoursSection = container.querySelector('span', { name: /Hours/i });
+
+        // --- verify elements exists on the page ---
+        expect(contactHeading).toBeTruthy();
+        expect(hoursSection).toBeTruthy();
+    })
+})
