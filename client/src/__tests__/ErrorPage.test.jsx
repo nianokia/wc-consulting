816c19cb58c4ec4ce7eaa52a6fca2273bf172383
Nia Wright <nw.niawright@gmail.com>
2024-11-04 18:10:09 -0500

    Create and develop client side tests

--- client/src/__tests__/ErrorPage.test.jsx
+++ client/src/__tests__/ErrorPage.test.jsx
@@ -1,21 +1,6 @@
 /**
  * ------ ERROR PAGE TEST ------
- * Render Heading ( √ )
+ * Render Header
+ * Render Heading
+ * Render Footer
  */
-import React from "react";
-import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
-import { MemoryRouter } from "react-router-dom";
-import ErrorPage from '../pages/ErrorPage.jsx';
-
-describe('ErrorPage page component', () => {
-    test('loads and displays heading (h1)', async () => {
-        render(<MemoryRouter><ErrorPage /></MemoryRouter>);
-
-        // --- select heading ---
-        const errorHeading = await screen.findByRole('heading', { name: /404 Not Found/i});
-
-        // --- verify heading exists on page ---
-        expect(errorHeading).toBeTruthy();
-    })
-})
--- client/src/__tests__/Home.test.jsx
+++ client/src/__tests__/Home.test.jsx
@@ -1,76 +1,11 @@
 /**
  * ------ HOME TEST ------
- * Render Video (  )
- * Render Services and About buttons (  )
- *  Check that buttons redirect properly (  )
- * Render Admin Login button (  )
- *  Check that Login redirects properly and logs user in (  )
+ * Render Header
+ * Render Hero
+ * Render Video
+ * Render Services and About buttons
+ * Check that buttons redirect properly
+ * Render Admin Login button
+ * Check that Login redirects properly and logs user in
+ * Render Footer
  */
-import React from "react";
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
-import { MemoryRouter } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-import Home from '../pages/Home.jsx';
-
-describe('Home page component', () => {
-    test('display Home page contents (headings, video)', async () => {
-        render(<MemoryRouter><Home /></MemoryRouter>);
-
-        // --- select elements ---
-        const backgroundHeading = await screen.findByRole('heading', { name: /Background Information addressing the What? and Why?/i });
-
-        // --- verify ---
-        expect(backgroundHeading).toBeTruthy();
-    })
-
-    test('link buttons redirect properly', async () => {
-        render(<MemoryRouter><Home /></MemoryRouter>);
-
-        // ------ SERVICES BUTTON ------
-        // --- select Services button ---
-        const servicesButton = await screen.findByRole('button', { name: /Services/i });
-        // --- verify Services page content doesn't currently exist ---
-        expect(screen.queryByRole('heading', { name: /Services/i })).not.toBeTruthy();
-        // --- click Services button ---
-        fireEvent.click(servicesButton);
-        // --- verify button redirects to Services page ---
-        await waitFor(() => {
-            expect(screen.findByRole('heading', { name: /Services/i })).toBeTruthy();
-        })
-
-        // ------ ABOUT BUTTON ------
-        // --- select About button ---
-        const aboutButton = await screen.findByRole('button', { name: /About/i });
-        // --- verify About page content doesn't currently exist ---
-        expect(screen.queryByRole('heading', { name: /About/i })).not.toBeTruthy();
-        // --- click About button ---
-        fireEvent.click(aboutButton);
-        // --- verify button redirects to About page ---
-        await waitFor(() => {
-            expect(screen.findByRole('heading', { name: /About/i })).toBeTruthy();
-        })
-    })
-
-    test('admin login button logs user in and redirects to AdminListEntries page', async () => {
-        render(<MemoryRouter><Home /></MemoryRouter>);
-
-        // --- import isAuthenticated Auth0 method ---
-        const { isAuthenticated } = useAuth0();
-
-        // --- select admin login button ---
-        const login = await screen.findByRole('button', { name: /Admin Login/i });
-        // --- verify AdminListEntries page content doesn't currently exist ---
-        expect(screen.queryByRole('heading', { name: /Admin List Entries/i })).not.toBeTruthy();
-        // --- click Admin Login button ---
-        fireEvent.click(login);
-        
-        if (isAuthenticated) {
-            // --- verify button redirects to AdminListEntries page if user authenticated ---
-            expect(screen.findByRole('heading', { name: /Admin List Entries/i })).toBeTruthy();
-        } else {
-            // --- verify button redirects to Home page if user not authenticated ---
-            expect(screen.findByRole('heading', { name: /Background Information addressing the What? and Why?/i })).toBeTruthy();
-        }
-    })
-})
