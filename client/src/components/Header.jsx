70b9179f32bb9a30ac73a03471756490a8bee0e0
Nia Wright <nw.niawright@gmail.com>
2024-11-05 16:54:04 -0500

    Improve accessibility with use of nav tag and useNavigate method

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 // PURPOSE - display a header at the top of every page with the business name and logo and a navbar of the company
 // its a reusable component that can be called in many components while being editted in one component (normalizes code)
 
 const Header = () => {
+  const navigate = useNavigate();
  return (
    <div className="Header" style={{display: "flex", alignItems: "flex-end", justifyContent: "space-between"}}>
-    <Link to='/' className="homeButton link-style" style={{display: "flex", flexDirection: "column", textDecoration: "none"}}>
+    <header onClick={() => navigate('/')} className="homeButton link-style" style={{display: "flex", flexDirection: "column", textDecoration: "none"}}>
       <div>
         <img src="../logo.png" alt="Wright Choice Consulting logo" style={{width: '40px'}} />
         <h4 style={{marginTop: "5px", marginBottom: 0}}>Wright Choice Consulting</h4>
@@ -15,15 +16,15 @@ const Header = () => {
       <div>
         <h6 style={{margin: "0px"}}>Family Therapy</h6>
       </div>
-    </Link>
-     <div className="Navbar" style={{display: "flex", gap: "10px"}}>
+    </header>
+     <nav className="Navbar" style={{display: "flex", gap: "10px"}}>
        <Link to='/about' className="link-style">About</Link>/
        <Link to='/services' className="link-style">Services</Link>/
        <Link to='/rates-and-insurance' className="link-style">Rates/ Insurance</Link>/
        <Link to='/faqs' className="link-style">FAQs</Link>/
        <Link to='/first-session' className="link-style">First Session</Link>/
        <Link to='/contact' className="link-style">Contact</Link>
-     </div>
+     </nav>
    </div>
  )
 }
