d89f5cc9338404f6ad151850601d9fac19f8d44c
Nia Wright <nw.niawright@gmail.com>
2024-11-05 16:55:47 -0500

    Improve layout and add content to FirstSession page

--- client/src/pages/FirstSession.jsx
+++ client/src/pages/FirstSession.jsx
@@ -2,35 +2,49 @@ import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // PURPOSE - Details what a client can expect at their first session.
 
 const FirstSession = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header />
       <Hero title="First Session" image='../walkbridge.png' />
-      <h1>First Session</h1>
 
       <div className="first-session-details">
-        Elementum aliquam vel pretium pretium sollicitudin nullam adipiscing sed egestas? Primis fermentum rutrum arcu porttitor torquent dis. Turpis molestie morbi amet metus vivamus eget luctus senectus. Netus vehicula habitant ante amet dui at felis rhoncus porttitor! Morbi nam laoreet; quis ridiculus duis lorem lacus. Orci risus justo leo dignissim ridiculus erat. Hendrerit porttitor massa a neque quam phasellus. Platea gravida senectus fusce pellentesque, ornare curae libero porttitor. Leo euismod maecenas est blandit ornare tristique etiam.
-
-        Potenti scelerisque sed scelerisque curae vel. Per turpis pharetra elit torquent potenti nisl mollis ligula. Non augue nisi orci himenaeos lectus interdum fringilla. Commodo torquent suspendisse proin platea justo erat convallis fames. Eros gravida id dignissim rutrum; volutpat curae. Quisque euismod iaculis curae amet at dictum. Praesent laoreet faucibus sed proin phasellus mi class turpis efficitur. Ligula porttitor tempor vel massa turpis ultricies et. Vitae mus quisque consectetur commodo ligula montes sagittis viverra.
-
-        Ac a pharetra; eu blandit eu condimentum. Tellus nisl pretium magnis vestibulum eleifend non. Consectetur natoque velit ultrices dapibus platea rhoncus mi? Commodo luctus semper at integer est praesent. Et duis pellentesque laoreet primis adipiscing; erat urna himenaeos. Nunc commodo quis taciti; lacus viverra id donec. Habitasse fermentum interdum per convallis congue fringilla nibh. Iaculis consectetur nulla aenean eleifend potenti praesent.
-
-        Eget lacinia libero ultricies netus cubilia iaculis. Nostra augue placerat vel phasellus, nullam rutrum. Pretium sodales proin sit volutpat urna mus dis lobortis. Vel quam platea mus massa vestibulum bibendum ante dictum. Sed fusce etiam odio lorem dignissim rutrum magna. Mi vestibulum mattis praesent pulvinar scelerisque hendrerit sagittis commodo! Facilisis semper metus parturient libero per hendrerit habitant aliquam. Vel penatibus ad vel curabitur turpis. Potenti lacus dignissim ligula, nisl donec integer nisl.
+        <p>
+          Your first session, will start out with you filling out consent paperwork that will take no longer than five to ten minutes. Often referred to as an intake session, is an opportunity for you and your therapist to get to know each other and establish a comfortable working relationship.
+        </p>
+        <p>
+          I normally refer to the first session as an interview.  You are interviewing me to make sure I have the skill set to bring value to your situation and would I be a good fit for you.  I am interviewing you to assess if I have the skill set / training to bring value to your problem.  
+        </p>
+        <p>
+          I will then review how long sessions last 53 minutes, therapy rate $150 per session, benefits and risks to counseling, confidentiality, being a mandated reported, how I handle suicidal / homicidal ideations, your role in therapy and my role in therapy. You will share your story on what brings you to therapy.
+        </p>
+        <p>
+        At the end of the first session I will offer your three options:
+        <ul>
+          <li>
+            Option #1:  Sign me up for another session, you are a good fit for me.
+          </li>
+          <li>
+            Option #2:  Let me think about this and I will text you back in a couple of days with a yes or a no to therapy.
+          </li>
+          <li>
+            Option #3: Thanks but no thanks Greg. I will continue to search for another therapist.  Your therapist will likely ask you about your concerns, your history, and your goals for counseling. They may also discuss confidentiality, fees, and the therapy approach they use.
+          </li>
+        </ul>
+        </p>
       </div>
-
       <br />
 
-      <button>
-        <Link to='/faqs'>FAQs</Link>
+      <button style={{float: 'right'}} onClick={() => navigate('/faqs')}>
+        FAQs
       </button>
       <br /><br />
 
-      <hr />
       <Footer />
     </>
   )
