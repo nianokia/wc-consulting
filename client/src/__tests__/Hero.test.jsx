b4fc34369db93a03f9af0172f594de59593fdb0d
Nia Wright <nw.niawright@gmail.com>
2024-11-05 12:07:10 -0500

    Linear gradient in backgroundImage style property is affecting test success. REVISIT later.

--- client/src/__tests__/Hero.test.jsx
+++ client/src/__tests__/Hero.test.jsx
@@ -12,15 +12,21 @@ import Hero from '../components/Hero.jsx';
 
 describe('Hero page component', () => {
     test('display heading and image', async () => {
-        render(<Hero title='Title' image='../walkbridge.png' />);
+        const imagePath = '../walkbridge.png'
+        render(<Hero title='Title' image={imagePath} textalign='start' />);
 
         // --- select elements ---
         const heroTitle = await screen.findByRole('heading', { name: this.title });
-        const heroImage = await screen.findByAltText('Image');
+
+        const heroDiv = document.querySelector('.Hero');
+        const styles = getComputedStyle(heroDiv);
 
         // --- verify element exists on page ---
         expect(heroTitle).toBeTruthy();
-        expect(heroImage.src).toBe(this.image);
+        console.log("Image prop: ", this.image);
+        // --- the test will only succeed if I put change the original file to not have a linear gradient and only have a background image ---
+        // --- I'll come back to this later and see if I want to reconfigure my file ---
+        expect(styles).toBe(`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)), url(${imagePath})`);
     })
 
     // --- if button exists check that it renders and redirects ---
