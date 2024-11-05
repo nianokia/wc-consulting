85c50d305e6c47ba02cc581763130bd9a578f476
Nia Wright <nw.niawright@gmail.com>
2024-11-05 17:00:35 -0500

    Stylize ContactQuestion and implement useNavigate

--- client/src/components/ContactQuestion.jsx
+++ client/src/components/ContactQuestion.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // PURPOSE - Displays question on ContactHome component to direct user to 2 the client or professional form
 
 const ContactQuestion = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <h2 style={{textAlign: "center"}}>Are you a client or professional?</h2>
+    <div className="ContactQuestion" style={{backgroundColor: "var(--secondary-color)", padding: "25px", margin: "30px 0"}}>
+      <h2 style={{textAlign: "center", color: 'var(--background-color)'}}>Are you a client or professional?</h2>
       <div style={{display: "flex", justifyContent: "space-evenly", marginBottom: "30px"}}>
-        <button>
-          <Link to='/contact/client'>Client</Link>
+        <button onClick={() => navigate('/contact/client')}>
+          Client
         </button>
-        <button>
-          <Link to='/contact/professional'>Professional</Link>
+        <button onClick={() => navigate('/contact/professional')}>
+          Professional
         </button>
       </div>
-    </>
+    </div>
   )
 }
 
