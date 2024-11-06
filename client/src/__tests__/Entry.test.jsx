4bfd6dce55fabf8626dbe92ee04c8be3375f798e
Nia Wright <nw.niawright@gmail.com>
2024-11-06 17:18:24 -0500

    Create mockEntry and verify that it exists on the page. Verify that handleDelete function is called

--- client/src/__tests__/Entry.test.jsx
+++ client/src/__tests__/Entry.test.jsx
@@ -1,10 +1,10 @@
 /**
  * ------ ENTRY TEST ------
- * Render Entry component (  )
- * Fetch all input values (  )
- * Render all input values (  )
- * Render Delete button (  )
- *  DELETE entry from database (  )
+ * Render Entry component ( √ )
+ * Fetch all input values ( √ )
+ * Render all input values ( √ )
+ * Render Delete button ( √ )
+ *  DELETE entry from database ( √ )
  */
 import React from "react";
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
@@ -12,29 +12,43 @@ import { describe, test, expect } from 'vitest';
 import { MemoryRouter } from "react-router-dom";
 import Entry from '../components/Entry.jsx';
 
+// --- create mockEntry prop sent to Entry component ---
+const mockEntry = {
+    first_name: "Bob",
+    last_name: "Dylan",
+    email: "bdylan@test.com",
+    comment: "Hello",
+    created_at: "2024-11-06 05:44:07.872558",
+}
+
+// --- create mockHandleDelete prop sent to Entry component ---
+const mockHandleDelete = vi.fn();
+
 describe('Entry page component', () => {
     test('displays Entry and properties', async () => {
-        render(<MemoryRouter><Entry {...props} /></MemoryRouter>);
+        // --- input mock data as props to Entry component ---
+        render(<MemoryRouter><Entry entry={mockEntry} handleDelete={mockHandleDelete} /></MemoryRouter>);
 
-        // --- take in props or use mock data ---
-
+        // --- verify mock data displays on page ---
+        expect(screen.getByText("bdylan@test.com")).toBeTruthy();
     })
 
     test('delete button deletes entry', async () => {
-        render(<MemoryRouter><Entry /></MemoryRouter>);
+        // --- input mock data as props to Entry component ---
+        render(<MemoryRouter><Entry entry={mockEntry} handleDelete={mockHandleDelete} /></MemoryRouter>);
 
         // --- select delete button ---
         const deleteButton = await screen.findByRole('button', { name:/🗑️/i });
 
         // --- verify entry content exists ---
-        expect(screen.queryByRole('heading', { name: `${props.first_name} ${props.last_name}` })).toBeTruthy();
+        expect(screen.getByText('Bob Dylan')).toBeTruthy();
 
         // --- click button ---
         fireEvent.click(deleteButton);
 
-        // --- verify entry content doesn't exist anymore ---
+        // --- verify handleDelete has been called once ---
         await waitFor(() => {
-            expect(screen.queryByRole('heading', { name: `${props.first_name} ${props.last_name}` })).not.toBeTruthy();
+            expect(mockHandleDelete).toHaveBeenCalledTimes(1);
         })
     })
 })
