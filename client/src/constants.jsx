927a80cd846dc4bc0d382de90f187405f559bd7b
Nia Wright <nw.niawright@gmail.com>
2024-11-11 04:44:09 -0500

    Add input-mapping variables and a capitalize function to the constants file.

--- client/src/constants.jsx
+++ client/src/constants.jsx
@@ -15,3 +15,41 @@ export const hideEmailError = () => {
    const emailError = document.getElementById("emailError");
    emailError.style.display = "none";
 }
+
+export const issueMapping = {
+   'depression': 'Depression',
+   'anxiety': 'Anxiety',
+   'stress': 'Stress',
+   'parenting': 'Parenting',
+   'grief_loss': 'Grief/ Loss',
+   'self_esteem': 'Self-Esteem',
+   'life_challenges': 'Life Challenges',
+   'anger': 'Anger Management',
+   'relationship': 'Relationship Difficulties',
+   'sports': 'Sports Perfomance'
+}
+
+export const raceMapping = {
+   'aian': 'American Indian or Alaska Native',
+   'asian': 'Asian',
+   'black': 'Black or African-American',
+   'pacific': 'Native Hawaiian or Other Pacific Islander',
+   'middle_eastern': 'Middle Eastern or North African',
+   'hispanic': 'Hispanic/ Latino',
+   'white': 'White',
+   'other': 'Other',
+   'prefer_not_to_say': 'Prefer not to say'
+}
+
+export const genderMapping = {
+   'man': 'Man',
+   'woman': 'Woman',
+   'non_binary': 'Non-binary',
+   'transgender': 'Transgender',
+   'other': 'Other',
+   'prefer_not_to_say': 'Prefer not to say'
+}
+
+export const capitalizeFirstLetter = (string) => {
+   return string.charAt(0).toUpperCase() + string.slice(1);
+}
