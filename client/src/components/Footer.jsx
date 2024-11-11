f722681ea850c9c8682192ef5b2c876ec6079dd8
Nia Wright <nw.niawright@gmail.com>
2024-11-11 04:36:13 -0500

    Stylize footer with MUI and make responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
+import { useMediaQuery, Box } from "@mui/material";
+
 /* ------ PURPOSE ------ 
   Display a footer at the end of every page with the name of the company.
   - a reusable component thats called in many components while being editted in one component (normalizes code)
 */
 
 const Footer = () => {
+  // --- MUI responsive breakpoints ---
+  const isMobile = useMediaQuery('(max-width: 450px)');
+  const isMidSize = useMediaQuery('(max-width: 760px)');
+
   return (
-    <>
+    <Box sx={{ maxWidth: isMobile ? 400 : isMidSize ? 760 : 1200, width: '100%' }}>
       <footer>Wright Choice Consulting</footer>
-    </>
+    </Box>
   )
 }
 
