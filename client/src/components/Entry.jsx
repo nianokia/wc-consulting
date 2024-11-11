1c52a05afd736e17a22e0452e1e588dad0e4c309
Nia Wright <nw.niawright@gmail.com>
2024-11-11 04:37:48 -0500

    Improve Entry styling to be more legible and responsive. Add greater distinction between client and professional entries.

--- client/src/components/Entry.jsx
+++ client/src/components/Entry.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import dayjs from 'dayjs';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
+import { issueMapping, raceMapping, genderMapping, capitalizeFirstLetter } from "../constants.jsx";
+
+// ------ MUI IMPORTS ------
+import { styled, Card, CardHeader, Box, Chip, Stack, Divider, Typography, IconButton, useMediaQuery } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
@@ -16,7 +11,39 @@ import DeleteIcon from '@mui/icons-material/Delete';
   Display each entry with details. Allow admin to delete an entry.
 */
 
+const ClientCard = styled(Card)(({ theme }) => ({
+  color: theme.palette.primary.darkest,
+  backgroundColor: theme.palette.primary.lightest,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.lighter,
+    color: theme.palette.primary.contrastText
+  },
+  borderRadius: 8,
+  border: `1px dotted ${theme.palette.primary.main}`,
+  margin: '10px auto',
+}));
+
+const ProfessionalCard = styled(Card)(({ theme }) => ({
+  color: theme.palette.secondary.darkest,
+  backgroundColor: theme.palette.secondary.lightest,
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.lighter,
+    color: theme.palette.secondary.darker
+  },
+  borderRadius: 8,
+  border: `1px dotted ${theme.palette.secondary.light}`,
+  margin: '10px auto'
+}));
+
+const StyledChip = styled(Chip)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  color: theme.palette.secondary.lightest,
+  margin: '4px'
+}))
+
 const Entry = ({ entry, handleDelete }) => {
+  const isMobile = useMediaQuery('(max-width: 450px)');
+  const isMidSize = useMediaQuery('(max-width: 760px)');
   // --- send selected entry to parent's handleDelete function ---
   const onDelete = (selectedEntry) => {
     handleDelete(selectedEntry);
@@ -27,47 +54,62 @@ const Entry = ({ entry, handleDelete }) => {
     return date;
   }
 
+  const capitalizedFName = capitalizeFirstLetter(entry.first_name);
+  const capitalizedLName = capitalizeFirstLetter(entry.last_name);
+  
   return (
     <>
     {entry.client_entry_id ? (
-      <Card variant="outlined" sx={{ maxWidth: 760, my: 1 }} style={{border: "3px solid #70A288"}}>
+      //  ------ CLIENT ENTRY ------
+      <ClientCard variant="outlined" sx={{ maxWidth: isMobile ? 350 : isMidSize ? 760 : 1200 }}>
         <Stack direction="row"
           sx={{ justifyContent: 'space-between', alignItems: 'center' }}
         >
-          <CardHeader title={`${entry.first_name} ${entry.last_name}`} subheader={entry.email} />
+          <CardHeader title={`${capitalizedFName} ${capitalizedLName}`} subheader={entry.email} />
           <Typography gutterBottom variant="body1" sx={{mx: '15px'}}>
             {formatDate(entry.created_at)}
           </Typography>
         </Stack>
         <Box sx={{ px: 2.5 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: isMobile ? 'none' : 'space-between', flexDirection: isMobile ? 'column' : 'row' }}>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Type of Therapy: {entry.type}
+              Therapy: {capitalizeFirstLetter(entry.type)}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
               Age: {entry.age}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Race: {entry.race}
+              Race: {raceMapping[entry.race]}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Gender: {entry.gender}
+              Gender: {genderMapping[entry.gender]}
             </Typography>
           </Box>
-          <Typography variant="body3" sx>
+          <Typography variant="body3">
             {entry.comment ? <p>{entry.comment}</p> : null}
           </Typography>
         </Box>
-        <Divider variant="middle" />
+        <Divider variant="middle" sx={{mt: '12px'}} />
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mx: '20px' }}>
           <Box sx={{ p: 2 }}>
             <Typography gutterBottom variant="body2">
               Issues
             </Typography>
-            <Stack direction="row" spacing={1}>
+            <Stack direction={isMobile ? 'column' : 'row'} spacing={1}>
               {entry.issues.split(',').map((issue) => {
                 const cleanedIssue = issue.replace(/['"]+/g, '').replace(/[{}]+/g, '');
-                return <Chip key={cleanedIssue} label={cleanedIssue} size="small" />;
+                return (
+                  <StyledChip 
+                    key={cleanedIssue}
+                    label={issueMapping[cleanedIssue]}
+                    size="small"
+                    sx={{
+                      margin: isMobile ? '4px 0' : '4px',
+                      display: 'flex',
+                      alignItems: 'center'
+                    }}
+                  />
+                );
               })}
             </Stack>
           </Box>
@@ -75,13 +117,14 @@ const Entry = ({ entry, handleDelete }) => {
             <DeleteIcon />
           </IconButton>
         </Box>
-      </Card>
+      </ClientCard>
     ) : (
-      <Card variant="outlined" sx={{ maxWidth: 760, my: 1 }} style={{border: "3px solid #DAB785"}}>
+      //  ------ PROFESSIONAL ENTRY ------ 
+      <ProfessionalCard variant="outlined" sx={{ maxWidth: isMobile ? 350 : isMidSize ? 760 : 1200 }}>
         <Stack direction="row"
           sx={{ justifyContent: 'space-between', alignItems: 'center' }}
         >
-          <CardHeader title={`${entry.first_name} ${entry.last_name}`} subheader={entry.email} />
+          <CardHeader title={`${capitalizedFName} ${capitalizedLName}`} subheader={entry.email} />
           <Typography gutterBottom variant="body1" sx={{mx: '15px'}}>
             {formatDate(entry.created_at)}
           </Typography>
@@ -92,7 +135,7 @@ const Entry = ({ entry, handleDelete }) => {
               Phone: {entry.phone}
             </Typography>
           </Box>
-          <Typography variant="body3" sx>
+          <Typography variant="body3">
             {entry.comment ? <p>{entry.comment}</p> : null}
           </Typography>
         </Box>
@@ -102,7 +145,7 @@ const Entry = ({ entry, handleDelete }) => {
             <DeleteIcon />
           </IconButton>
         </Box>
-      </Card>
+      </ProfessionalCard>
     )}      
     </>
   )
