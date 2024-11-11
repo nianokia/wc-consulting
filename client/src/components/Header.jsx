00723cde8f15ba21f0737f9aa6b6a011eb0955f3
Nia Wright <nw.niawright@gmail.com>
2024-11-11 04:34:35 -0500

    Stylize Header/ NavBar with MUI and make it responsive with Menu Icon on smaller devices. Import transparent logo image.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,34 +1,104 @@
-import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import { useMediaQuery, Box, Typography, ButtonGroup, Button, Menu, MenuItem, IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 /* ------ PURPOSE ------
   Display a header at the top of every page with the business name and logo and a navbar of the company
   - a reusable component thas called in many components while being editted in one component (normalizes code)
 */
+
 const Header = () => {
   const navigate = useNavigate();
+
+  // --- MUI responsive breakpoints ---
+  const isMobile = useMediaQuery('(max-width: 450px)');
+  const isMidSize = useMediaQuery('(max-width: 760px)');
+
+  // --- MUI navBar collapse ---
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  }
+
+  const pages = [
+    {title: 'About', link: '/about'},
+    {title: 'Services', link: '/services'},
+    {title: 'Rates', link: '/rates-and-insurance'},
+    {title: 'FAQs', link: '/faqs'},
+    {title: 'First Session', link: '/first-session'},
+    {title: 'Contact', link: '/contact'}
+  ];
+
  return (
-   <div className="Header" style={{display: "flex", alignItems: "flex-end", justifyContent: "space-between"}}>
-    <header onClick={() => navigate('/')} className="homeButton link-style" style={{display: "flex", flexDirection: "column", textDecoration: "none"}}>
-      <div>
-        <img src="../logo.png" alt="Wright Choice Consulting logo" style={{width: '40px'}} />
-        <h4 style={{marginTop: "5px", marginBottom: 0}}>Wright Choice Consulting</h4>
-      </div>
-      <div>
-        <h6 style={{margin: "0px"}}>Family Therapy</h6>
-      </div>
-    </header>
-     <nav className="Navbar" style={{display: "flex", gap: "10px"}}>
-       <Link to='/about' className="link-style">About</Link>/
-       <Link to='/services' className="link-style">Services</Link>/
-       <Link to='/rates-and-insurance' className="link-style">Rates/ Insurance</Link>/
-       <Link to='/faqs' className="link-style">FAQs</Link>/
-       <Link to='/first-session' className="link-style">First Session</Link>/
-       <Link to='/contact' className="link-style">Contact</Link>
+   <Box className='Header' sx={{ maxWidth: isMobile ? 400 : isMidSize ? 760 : 1200, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Box onClick={() => navigate('/')} sx={{ display: 'flex', alignItems: 'center' }}>
+        <img src='../logo-transparent.png' alt='Wright Choice Consulting logo' style={{width: '50px'}} />
+        <Box sx={{ display: isMobile ? 'block' : 'flex', flexDirection: 'column' }}>
+          <Typography variant='h4' sx={{ display: isMobile ? 'block' : 'block', mt: '5px', ml: 1, mb: 0, fontSize: isMobile ? '16px' : '18px', color: 'secondary.darker' }}
+          >
+            Wright Choice Consulting
+          </Typography>
+          <Typography variant='h6' sx={{ display: isMobile ? 'none' : 'block', ml: 1, fontSize: '16px', color: 'secondary.darker' }}>
+            Family Therapy
+          </Typography>
+        </Box>
+      </Box>
+     <nav className='Navbar' position='static' sx={{display: 'flex', gap: '1', color: 'secondary.main'}}>
+      {isMidSize ? (
+        <Box>
+          <IconButton
+            size="large"
+            aria-label="collapsed site navigation"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{ display: isMobile ? 'block' : 'none' }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page.title} onClick={() => {
+                handleCloseNavMenu();
+                navigate(page.link);
+              }}>
+                <Typography sx={{ textAlign: 'center' }}>{page.title}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      ) : (
+        <ButtonGroup variant='text' aria-label='text button group' sx={{ color: 'secondary.main' }}>
+          {pages.map((page) => (
+            <Button onClick={() => navigate(page.link)} sx={{ color: 'secondary.main' }}>
+              {page.title}
+            </Button>
+          ))}
+        </ButtonGroup>
+      )}
      </nav>
-   </div>
+   </Box>
  )
 }
 
 export default Header
-
