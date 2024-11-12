f2fe5ff586a92b41d176f0738f5422172f531619
Nia Wright <nw.niawright@gmail.com>
2024-11-12 04:13:46 -0500

    Add PsychWidget to About page below Contact button.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -69,16 +69,16 @@ const About = () => {
             marginRight: isMidSize ? '0' : '100px'
           }}>
             <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
-            Gregory B. Wright is a Licensed Professional Counselor Supervisor and a seasoned mental health professional with over <strong>20 years of experience</strong>. He specializes in counseling individuals, families, and groups facing a variety of challenges, including youth disruptive behaviors, adults struggling with life challenges, and parenting coaching.
+              Gregory B. Wright is a Licensed Professional Counselor Supervisor and a seasoned mental health professional with over <strong>20 years of experience</strong>. He specializes in counseling individuals, families, and groups facing a variety of challenges, including youth disruptive behaviors, adults struggling with life challenges, and parenting coaching.
             </Typography>
             <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
-            As the sole proprietor of Wright Choice Consulting, LLC, Gregory addresses a wide range of mental health concerns and crises, i.e. Criminal Domestic Violence and Anger Management Groups. The main goal of this practice is to facilitate transformative therapy to promote healthy coping mechanisms and conflict resolution.
+              As the sole proprietor of Wright Choice Consulting, LLC, Gregory addresses a wide range of mental health concerns and crises, i.e. Criminal Domestic Violence and Anger Management Groups. The main goal of this practice is to facilitate transformative therapy to promote healthy coping mechanisms and conflict resolution.
             </Typography>
             <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
-            Gregory holds a Bachelor's degree in Sociology from the State University of New York at Brockport and a Master's degree in Counseling from Webster University. 
+              Gregory holds a Bachelor's degree in Sociology from the State University of New York at Brockport and a Master's degree in Counseling from Webster University.
             </Typography>
             <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
-            Prior to his private practice, Gregory served as a certified trainer for Therapeutic Crisis Intervention, providing invaluable training to a high management group home. His extensive experience and dedication to his clients make him a valuable resource for those seeking support and guidance.
+              Prior to his private practice, Gregory served as a certified trainer for Therapeutic Crisis Intervention, providing invaluable training to a high management group home. His extensive experience and dedication to his clients make him a valuable resource for those seeking support and guidance.
             </Typography>            
           </article>
 
@@ -94,11 +94,14 @@ const About = () => {
             mt: 4
           }}>
             Contact
-          </button>
-          {/* PSYCHOLOGY TODAY BUTTON */}
-        </div>
-      </div>
-      
+          </Button>
+          <br />
+
+          <PsychWidget />
+          
+        </Box>
+      </Box>
+
       <Footer />
     </Container>
   )
