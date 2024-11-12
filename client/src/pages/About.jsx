301c9e75032a198a7283e1e7a7f4bbc0b3c7b579
Nia Wright <nw.niawright@gmail.com>
2024-11-12 04:05:46 -0500

    Integrate MUI to stylize and make About page responsive on 'sm' & 'xl' devices.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,39 +2,97 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
+import PsychWidget from "../components/PsychWidget";
 
-// PURPOSE - Provides background information about the owner 
+// ------ MUI IMPORTS ------
+import { useMediaQuery, Container, Box, Typography, Button } from "@mui/material";
+
+/* ------ PURPOSE ------
+  Provides background information about the owner 
+*/
 
 const About = () => {
   const navigate = useNavigate();
-  return (
-    <>
-      <Header />
-      <h1>About</h1>
-      <hr />
-      <div className="aboutContent" style={{display: "flex", gap: "20px"}}>
-        <div className="name-and-headshot">
-            <img src="../headshot.jpeg" alt="Headshot of Gregory B Wright in front of a black background. He's a black man wearing glasses, a black blazer, a white buttondown shirt, and a paid navy and blue tie." />
-            <h2 style={{marginBottom: "6px", textAlign: "end"}}>
-              Gregory B Wright
-            </h2>
-            <h3 style={{marginTop: "0", textAlign: "end"}}>
-              LPCS - Sole Proprietor
-            </h3>
-        </div>
-        <div className="about-text-content" style={{display: "flex", flexDirection: "column", alignItems: "center",}}>
-          <article style={{fontSize: "18px", textAlign: "justify"}}>
-            Gregory B. Wright is a Licensed Professional Counselor Supervisor and a seasoned mental health professional with over <strong>20 years of experience</strong>. He specializes in counseling individuals, families, and groups facing a variety of challenges, including youth disruptive behaviors, adults struggling with life challenges, and parenting coaching.
-            <br /><br />
-            As the sole proprietor of Wright Choice Consulting, LLC, Gregory addresses a wide range of mental health concerns and crises, i.e. Criminal Domestic Violence and Anger Management Groups. The main goal of this practice is to facilitate transformative therapy to promote healthy coping mechanisms and conflict resolution.
-            <br /><br />
-            Gregory holds a Bachelor's degree in Sociology from the State University of New York at Brockport and a Master's degree in Counseling from Webster University. 
-            <br /><br />
-            Prior to his private practice, Gregory served as a certified trainer for Therapeutic Crisis Intervention, providing invaluable training to a high management group home. His extensive experience and dedication to his clients make him a valuable resource for those seeking support and guidance.
 
+  // --- MUI responsive breakpoints ---
+  const isMobile = useMediaQuery('(max-width: 450px)');
+  const isMidSize = useMediaQuery('(max-width: 760px)');
+
+  return (
+    <Container id='About' sx={{
+      maxWidth: isMobile ? 400 : isMidSize ? 1200 : 1600,
+      minWidth: isMobile ? 350 : isMidSize ? 760 : 1660, 
+      width: '100%', 
+    }}>
+      <Header />
+      <Typography variant='h1' sx={{ fontSize: isMobile ? '36px' : '48px', mt: 2 }}>
+        About
+      </Typography>
+      <hr />
+      <Box id='aboutContent' sx={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row', gap: isMobile ? '0px' : '20px' }}>
+        <Box id='headshotAndTitle' sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignSelf: 'center',
+          m: isMobile ? '0 auto' : '0',
+          pl: isMobile ? 'auto' : isMidSize ? 'auto' : 7,
+          width: isMobile ? 'none' : '35%'
+        }}>
+            <img src='../headshot.png' alt="Headshot of Gregory B Wright in front of a black background. He's a black man wearing glasses, a black blazer, a white buttondown shirt, and a paid navy and blue tie." width={isMobile ? '260px' : isMidSize ? '320px' : '410px'} />
+            <Typography variant='h2' sx={{
+              my: 1,
+              mr: isMidSize ? '0px' : '140px',
+              textAlign: 'end',
+              fontSize: isMobile ? '24px' : '36px'
+            }}>
+              Gregory B Wright
+            </Typography>
+            <Typography variant='h3' sx={{
+              mt: 0,
+              mr: isMidSize ? '0px' : '140px',
+              textAlign: 'end',
+              fontSize: isMobile ? '14px' : '22px'
+            }}>
+              LPCS - Sole Proprietor
+            </Typography>
+        </Box>
+        <Box id="biography" sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: isMobile ? 'auto' : '65%'
+        }}>
+          <article style={{
+            fontSize: '18px',
+            textAlign: 'justify',
+            margin: isMobile ? '10px' : 'auto',
+            marginRight: isMidSize ? '0' : '100px'
+          }}>
+            <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
+            Gregory B. Wright is a Licensed Professional Counselor Supervisor and a seasoned mental health professional with over <strong>20 years of experience</strong>. He specializes in counseling individuals, families, and groups facing a variety of challenges, including youth disruptive behaviors, adults struggling with life challenges, and parenting coaching.
+            </Typography>
+            <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
+            As the sole proprietor of Wright Choice Consulting, LLC, Gregory addresses a wide range of mental health concerns and crises, i.e. Criminal Domestic Violence and Anger Management Groups. The main goal of this practice is to facilitate transformative therapy to promote healthy coping mechanisms and conflict resolution.
+            </Typography>
+            <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
+            Gregory holds a Bachelor's degree in Sociology from the State University of New York at Brockport and a Master's degree in Counseling from Webster University. 
+            </Typography>
+            <Typography variant='body1' sx={{ my: 3.5, fontSize: isMobile ? '16px' : '20px' }}>
+            Prior to his private practice, Gregory served as a certified trainer for Therapeutic Crisis Intervention, providing invaluable training to a high management group home. His extensive experience and dedication to his clients make him a valuable resource for those seeking support and guidance.
+            </Typography>            
           </article>
-          <br /><br />
-          <button onClick={() => navigate('/contact')} style={{width: "25%"}}>
+
+          {/* ------ CONTACT BUTTON ------ */}
+          <Button variant='contained' onClick={() => navigate('/contact')} sx={{
+            backgroundColor: 'accent.main',
+            color: 'accent.darkest',
+            '&:hover': {
+              backgroundColor: 'accent.light',
+              color: 'accent.darkest',
+            },
+            width: '25%',
+            mt: 4
+          }}>
             Contact
           </button>
           {/* PSYCHOLOGY TODAY BUTTON */}
@@ -42,7 +100,7 @@ const About = () => {
       </div>
       
       <Footer />
-    </>
+    </Container>
   )
 }
 
