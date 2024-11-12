96ba85e9c3d85f0ced7979665b96b4ce08081f0b
Nia Wright <nw.niawright@gmail.com>
2024-11-12 02:56:35 -0500

    Integrate MUI to stylize FAQs and make responsive on 'sm' & 'xl' devices.

--- client/src/pages/Faqs.jsx
+++ client/src/pages/Faqs.jsx
@@ -3,103 +3,272 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
 
-// PURPOSE - Displays list of FAQs with a table of contents that jumps to that specific question on the page
+// ------ MUI IMPORTS ------
+import { styled, useMediaQuery, Container, Box, Typography, Button, List, ListItem, ListItemIcon, Link } from '@mui/material';
+import { PlayArrow } from "@mui/icons-material";
+
+// --- custom MUI components ---
+const StyledButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.secondary.lightest,
+  backgroundColor: theme.palette.secondary.main,
+  border: '1px dotted theme.palette.secondary.darkest',
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.lighter,
+    color: theme.palette.tertiary.darkest
+  },
+  borderRadius: 8
+}))
+
+/* ------ PURPOSE ------
+  Displays list of FAQs with a table of contents that jumps to that specific question on the page
+*/
 
 const Faqs = () => {
   const navigate = useNavigate();
+
+  // --- MUI responsive breakpoints ---
+  const isMobile = useMediaQuery('(max-width: 450px)');
+  const isMidSize = useMediaQuery('(max-width: 760px)');
+
   return (
-    <>
+    <Container sx={{ 
+      maxWidth: isMobile ? 400 : isMidSize ? 1200 : 1600,
+      minWidth: isMobile ? 350 : isMidSize ? 760 : 1660, 
+      width: '100%', 
+    }} >
+
       <Header />
       <hr />
 
-      <div className="faqContent" style={{display: "flex", gap: "20px", marginTop: "30px"}}>
-        <div className="faq-col" style={{width: "25%"}}>
-          <h1>FAQs</h1>
-          <section>
-            <h4>Questions</h4>
-            <ul style={{listStyle: "none", paddingLeft: "0"}}>
+      <Box id='faqContent' sx={{
+        display: 'flex',
+        flexDirection: isMobile ? 'column' : 'row',
+        gap: isMidSize ? '20px' : '65px',
+        m: '30px auto'
+      }}>
+        <Box id='tableOfContents' sx={{ width: isMobile ? '100%' : '33%', ml: isMidSize ? 0 : 2}}>
+          <Typography variant='h1' sx={{
+            fontSize: isMobile ? '30px' : isMidSize ? '30px' : '45px',
+            mt: isMobile ? 0 : 2,
+            mb: 2,
+            ml: isMobile ? 2 : 1
+          }}>
+            FAQS
+          </Typography>
+          <Box sx={{ backgroundColor: 'accent.light', py: 1.5, justifyItems: 'center' }}>
+            <Typography variant='h3' sx={{
+              fontSize: isMobile ? '18px' : '22px',
+              px: isMidSize ? 2 : 3,
+              pt: isMidSize ? 1 : 2,
+              justifySelf: 'start'
+            }}>
+              Questions
+            </Typography>
+            <List sx={{ listStyle: 'none', pl: '0' }}>
               {/* Jumps to the specific question on the page onclick */}
-              <li style={{marginBottom: "8px"}}><a href="#q1">What is mental health counseling?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q2">Who can benefit from mental health counseling?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q3">How does mental health counseling work?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q4">How long does mental health counseling take?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q5">Is mental health counseling confidential?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q6">What should I look for in a mental health counselor?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q7">How do I know if mental health counseling is right for me?</a></li>
-              <li style={{marginBottom: "8px"}}><a href="#q8">Additional Resources</a></li>
-            </ul>
-            <button onClick={() => navigate('/contact')}>
-              Contact
-            </button>
-          </section>
-        </div>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q1' sx={{ color: 'secondary.main' }}>
+                  What is mental health counseling?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q2' sx={{ color: 'secondary.main' }}>
+                  Who can benefit from mental health counseling?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q3' sx={{ color: 'secondary.main' }}
+                  >How does mental health counseling work?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q4' sx={{ color: 'secondary.main' }}>
+                  How long does mental health counseling take?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q5' sx={{ color: 'secondary.main' }}>
+                  Is mental health counseling confidential?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q6' sx={{ color: 'secondary.main' }}>
+                  What should I look for in a mental health counselor?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q7' sx={{ color: 'secondary.main' }}>
+                  How do I know if mental health counseling is right for me?
+                </Link>
+              </ListItem>
+              <ListItem sx={{ marginBottom: '8px' }}>
+                <Link href='#q8' sx={{ color: 'secondary.main' }}>
+                Additional Resources</Link>
+              </ListItem>
+            </List>
+            {/* ------ CONTACT BUTTON ------ */}
+            {isMobile ? (
+                null
+              ) : (
+                <StyledButton onClick={() => navigate('/contact')} sx={{ mb: 2 }}>
+                  Contact
+                </StyledButton>
+              )
+            }
+          </Box>
+        </Box>
 
-        <div className="faq-col" style={{width: "75%"}}>
+        <Box className="faq-col" style={{ width: isMobile ? '100%' : '66%', justifyItems: 'end' }}>
           <article>
-            <section className="faq-qa" id="q1">
-              <h3>What is mental health counseling?</h3>
-              <p>
+            <Box className="faq-qa" id="q1" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                What is mental health counseling?
+              </Typography>
+              <Typography variant="body1">
                 Mental health counseling, also known as psychotherapy or talk therapy, is a collaborative process between a trained mental health professional and an individual seeking support for their emotional, psychological, or behavioral well-being. It involves open communication, active listening, and the development of coping strategies and skills to address a range of mental health concerns.
-              </p>
-            </section>
-            <section className="faq-qa" id="q2">
-              <h3>Who can benefit from mental health counseling?</h3>
-              <p>
-                Anyone can benefit from mental health counseling, regardless of age, background, or life circumstances. 
-              </p>
-            </section>
-            <section className="faq-qa" id="q3">
-              <h3>How does mental health counseling work?</h3>
-              <p>
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q2" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                Who can benefit from mental health counseling?
+              </Typography>
+              <Typography variant="body1">
+                Anyone can benefit from mental health counseling, regardless of age, background, or life circumstances.
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q3" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                How does mental health counseling work?
+              </Typography>
+              <Typography variant="body1">
                 The counseling process typically involves regular sessions with a therapist. During these sessions, you'll have the opportunity to discuss your thoughts, feelings, and experiences in a safe and supportive environment. Your therapist will listen attentively, ask questions, and offer guidance and feedback. Together, you'll work towards identifying and addressing the underlying causes of your concerns and develop strategies to improve your overall well-being.
-              </p>
-            </section>
-            <section className="faq-qa" id="q4">
-              <h3>How long does mental health counseling take?</h3>
-              <p>
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q4" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                How long does mental health counseling take?
+              </Typography>
+              <Typography variant="body1">
                 The duration of mental health counseling varies depending on individual needs and goals. Some individuals may benefit from short-term counseling, while others may require longer-term therapy. The frequency of sessions can also vary, with some individuals meeting weekly, bi-weekly, or monthly.
-              </p>
-            </section>
-            <section className="faq-qa" id="q5">
-              <h3>Is mental health counseling confidential?</h3>
-              <p>
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q5" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                Is mental health counseling confidential?
+              </Typography>
+              <Typography variant="body1">
                 Yes, mental health counseling is generally confidential. I am legally and ethically bound to keep your information private, except in specific circumstances, such as: disclosure of children, vulnerable adults or animals being physically, emotionally, sexually, emotionally, financially abused or neglected. Also, a duty to warn the general community if you make threats of harm that I deem credible.  
-
-              </p>
-            </section>
-            <section className="faq-qa" id="q6">
-              <h3>What should I look for in a mental health counselor?</h3>
-              <p>
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q6" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                What should I look for in a mental health counselor?
+              </Typography>
+              <Typography variant="body1">
                 When choosing a counselor, consider the following:
-              </p>
-              <ul>
-                <li>Credentials: Ensure your therapist is licensed and qualified in their field.</li>
-                <li>Experience: Look for a therapist with experience in addressing your specific concerns.</li>
-                <li>Rapport: Choose a therapist with whom you feel comfortable, it's a good fit and you can establish a trusting relationship.</li>
-                <li>Approach: Consider whether you prefer a particular therapeutic approach, such as cognitive-behavioral therapy (CBT), psychodynamic therapy, or solution focus brief  therapy.</li>
-              </ul>
-            </section>
-            <section className="faq-qa" id="q7">
-              <h3>How do I know if mental health counseling is right for me?</h3>
-              <p>
+              </Typography>
+              <List sx={{ 
+                listStyleType: 'none',
+                '& li': {
+                  paddingLeft: isMobile ? '10px' : '16px',
+                  position: 'relative',
+                }
+              }}>
+                <ListItem key='faqQ6Item1'>
+                  <ListItemIcon key='optionIcon1'>
+                    <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                  </ListItemIcon>
+                  Credentials: Ensure your therapist is licensed and qualified in their field.
+                </ListItem>
+                <ListItem key='faqQ6Item2'>
+                  <ListItemIcon key='optionIcon2'>
+                    <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                  </ListItemIcon>
+                  Experience: Look for a therapist with experience in addressing your specific concerns.
+                </ListItem>
+                <ListItem key='faqQ6Item3'>
+                  <ListItemIcon key='optionIcon3'>
+                    <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                  </ListItemIcon>
+                  Rapport: Choose a therapist with whom you feel comfortable, it's a good fit and you can establish a trusting relationship.
+                </ListItem>
+                <ListItem key='faqQ6Item4'>
+                  <ListItemIcon key='optionIcon4'>
+                    <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                  </ListItemIcon>
+                  Approach: Consider whether you prefer a particular therapeutic approach, such as cognitive-behavioral therapy (CBT), psychodynamic therapy, or solution focus brief  therapy.
+                </ListItem>
+              </List>
+            </Box>
+            <Box className="faq-qa" id="q7" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                How do I know if mental health counseling is right for me?
+              </Typography>
+              <Typography variant="body1">
                 If you're struggling with your mental health, feeling overwhelmed, or experiencing persistent negative emotions, mental health counseling may be a valuable option. It's important to remember that seeking help is a sign of strength, not weakness.
-              </p>
-            </section>
-            <section className="faq-qa" id="q8">
-              <h3>Additional Resources:</h3>
-              <ul>
-                <li><a href="https://www.nami.org/" target="_blank">National Alliance on Mental Illness (NAMI)</a></li>
-                <li><a href="https://store.samhsa.gov/" target="_blank">MentalHealth.gov</a></li>
-                <li>Crisis Text Line: Text HOME to 741741</li>
-                <li>988 Suicide & Crisis Lifeline: Call or text 988</li>
-              </ul>
-            </section>
+              </Typography>
+            </Box>
+            <Box className="faq-qa" id="q8" sx={{ my: 3 }} >
+              <Typography variant='h5' sx={{ mb: 1.25 }}>
+                Additional Resources:
+              </Typography>
+              <Typography variant="body1">
+                <List sx={{
+                  listStyleType: 'none',
+                  '& li': {
+                    paddingLeft: isMobile ? '10px' : '16px',
+                    position: 'relative',
+                  }
+                }}>
+                  <ListItem key='faqQ8Item1'>
+                    <ListItemIcon key='optionIcon1'>
+                      <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                    </ListItemIcon>
+                    <Link href="https://www.nami.org/" target="_blank" rel='noreferrer' sx={{
+                      color: 'secondary.main'
+                    }}>
+                      National Alliance on Mental Illness (NAMI)
+                    </Link>
+                  </ListItem>
+                  <ListItem key='faqQ8Item2'>
+                     <ListItemIcon key='optionIcon1'>
+                      <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                    </ListItemIcon>
+                    <Link href="https://store.samhsa.gov/" target="_blank" rel='noreferrer' sx={{
+                      color: 'secondary.main'
+                    }}>
+                      MentalHealth.gov
+                    </Link>
+                  </ListItem>
+                  <ListItem key='faqQ8Item3'>
+                     <ListItemIcon key='optionIcon1'>
+                      <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                    </ListItemIcon>
+                    Crisis Text Line: Text HOME to 741741
+                  </ListItem>
+                  <ListItem key='faqQ8Item4'>
+                     <ListItemIcon key='optionIcon1'>
+                      <PlayArrow sx={{ color: 'primary.main', fontSize: '32px' }} />
+                    </ListItemIcon>
+                    988 Suicide & Crisis Lifeline: Call or text 988
+                  </ListItem>
+                </List>
+              </Typography>
+            </Box>
           </article>
-        </div>
-      </div>
+          {isMobile ? (
+                <StyledButton onClick={() => navigate('/contact')}>
+                  Contact
+                </StyledButton>
+              ) : (
+                null
+              )
+            }
+        </Box>
+      </Box>
       
       <Footer />
-    </>
+    </Container>
   )
 }
 
